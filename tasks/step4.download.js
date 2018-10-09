acf6396e258cc54c1813df7975a686953a3eaab2
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2018-10-09 13:17:27 +0530

    Update step4.download.js

--- tasks/step4.download.js
+++ tasks/step4.download.js
@@ -6,12 +6,12 @@ module.exports = (data) => {
     return new Promise((resolve, reject) => {
         data.forEach((g, gidx, garr) => {
             g.photos.forEach((p, pidx, parr) => {
-                console.log('Downloading ' + p.fileId);
+                console.log('Downloading ' + g.id + ' - ' + p.fileId);
                 request.get(p.fileUrl)
                     .on('error', (err) => { console.error("ERROR: " + err) })
                     .pipe(fs.createWriteStream('temp/' + p.fileId + '.jpg'))
                     .on('close', () => {
-                        console.log('Finished Downloading ' + p.fileId);                        
+                        console.log('Finished Downloading ' + g.id + ' - ' + p.fileId);                       
                         if (gidx === garr.length - 1 && pidx === parr.length - 1) {
                             setTimeout(() => {
                                 console.log("Download task completed @ " + moment().utcOffset("+05:30").format('YYYY-MM-DD hh:mm:ss a'));
